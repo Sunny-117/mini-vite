d299173293a3c346472be9c3eac6b6906631f704
Sunny-117 <zhiqiangfu6@gmail.com>
2024-05-06 16:25:19 +0800

    feat: plugin

--- packages/plugin/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// const presetEnv = require('postcss-preset-env');
-// module.exports = {
-//   plugins: [
-//     presetEnv({
-//       browsers: ['last 2 versions', '> 1%', 'IE 11'],
-//       autoprefixer: { grid: true } 
-//     })
-//   ]
-// }
